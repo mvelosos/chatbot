ad6a26d857eed467c8d7b112a3e2484a9c772c58
Mateus Veloso <mfvelosos@gmail.com>
2019-01-09 23:16:53 -0300

    Database, rake, gem, app settings

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'sinatra'
+require 'sinatra/activerecord'
+  
+require './config/database'
+  
+class App < Sinatra::Base
+
+    get '/' do
+      'Hello world!'
+    end
+    
+  end
