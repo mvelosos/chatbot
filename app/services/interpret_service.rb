20b4f8dcdb00f48315045cc1d4bef053e9e8b3f4
Mateus Veloso <mfvelosos@gmail.com>
2019-02-23 20:49:48 -0300

    Updated list service and created create service for checklist

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -3,17 +3,19 @@ class InterpretService
     def self.call action, params
       case action
       when "list", "search", "search_by_hashtag"
-        FaqModule::ListService.new(params, action).call()
+        FaqModule::ListService.new(params, action).call
       when "create"
-        FaqModule::CreateService.new(params).call()
+        FaqModule::CreateService.new(params).call
       when "remove"
-        FaqModule::RemoveService.new(params).call()
+        FaqModule::RemoveService.new(params).call
       when "help"
-        HelpService.call()
+        HelpService.call
       when "list_language"
-        ListLanguageService.call()
+        ListLanguageService.call
       when "list_checklist"
-        ChecklistModule::ListService.call()
+        ChecklistModule::ListService.call
+      when "add_checklist"
+        ChecklistModule::CreateService.new(params, action).call
       else
         "Não compreendi o seu desejo"
       end
