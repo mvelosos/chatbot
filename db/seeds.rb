ee4904678845202ed655676fb21f023c4fe5ada1
Mateus Veloso <mfvelosos@gmail.com>
2019-02-24 16:34:56 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
-Checklist.create(description: 'Ir comer um hamburguer', done: true)
-Checklist.create(description: 'Ir pra casa de mozão', done: false)
+# Checklist.create(description: 'Ir comer um hamburguer', done: true)
+# Checklist.create(description: 'Ir pra casa de mozão', done: false)
 Checklist.create(description: 'Assistir HP com mozão', done: false)
