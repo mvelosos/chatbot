680a8d27604f610ac3dcf1bab2b83b22db8a6be7
Mateus Veloso <mfvelosos@gmail.com>
2019-02-24 16:30:25 -0300

    hotfix

--- app/services/checklist/list_service.rb
+++ app/services/checklist/list_service.rb
@@ -5,9 +5,14 @@ module ChecklistModule
       def self.call
         if Checklist.any?
           list = Checklist.all
+
+          response = "--- Checklist ---\n\n"
           list.each do |l|
-            l.description
+            if !l.done?
+              response += "#{l.description}\n"
+            end
           end
+          response += "\n\n"
         else
           'Para adicionar um item digite: To Do'
         end
