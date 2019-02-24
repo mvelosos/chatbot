e1243aaac759a5a622ffd6fbb160904e00a55109
Mateus Veloso <mfvelosos@gmail.com>
2019-02-24 16:24:39 -0300

    hotfix

--- app/services/checklist/list_service.rb
+++ app/services/checklist/list_service.rb
@@ -2,22 +2,13 @@ module ChecklistModule
 
     class ListService
 
-        def call
-            if Checklist.any?
-                list = Checklist.all
-
-                response = "--- Checklist ---\n\n"
-                list.each do |l|
-                    if l.done
-                        'funcionou'
-                        # response += "#{l.description}\n"
-                    end
-                end
-                response += "\n\n"
-            else
-                'Para adicionar um item digite: To Do'
-            end
+      def call
+        if Checklist.any?
+          'Tem coisa'
+        else
+          'Para adicionar um item digite: To Do'
         end
+      end
 
     end
 end
